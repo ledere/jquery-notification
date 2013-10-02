d956862c109acf6814724db0dd1e21cefceda6c3
Frank Huffener <frank@duck.local>
2013-10-02 21:49:26 +0200

    built the package, right

---
+++ dist/jquery.notification.js
@@ -0,0 +1,63 @@
+/*! notification - v0.0.2 - 2013-10-02
+* https://github.com/ledere/notification
+* Copyright (c) 2013 Frank Huffener; Licensed MIT */
+(function ($) {
+    
+    // default settings
+     var oDefaults = {
+        style: 'toast',
+        cssClass: 'fh-notification', // override the css class to use your own styling
+        text: '',
+        autoHide: true,
+        autoHideDelay: 2000, // in milliseconds
+        animationSpeed: 500 // in milliseconds
+     };
+    
+    var self = {};
+    
+    self.show = function($elem, oData) {
+    
+        // Create the message
+        var $message = $("<div></div>").addClass(oData.cssClass);
+        var $text = $("<p></p>").text(oData.text);
+        $text.appendTo($message);
+
+        /*
+         * Toast message
+         * Yeah, it's awesome
+         */
+        if(oData.style === "toast") {
+            $message.addClass("toast").appendTo($elem).fadeIn(oData.animationSpeed, function() {
+                if(oData.autoHide) {
+                    // hide the message
+                    $(this).delay(oData.autoHideDelay).fadeOut(oData.animationSpeed, function() {
+                        $(this).remove();
+                    });
+
+                } else {
+                    // bind click to hide the message, etc, etc
+                    $message.addClass("clickable").one("click", function() {
+                        $(this).fadeOut(oData.animationSpeed, function() {
+                            $(this).remove();
+                        });
+                    });
+                }
+            });
+        }
+        
+        
+    };
+
+    // Collection method.
+    $.fn.notification = function (oData) {
+        // extend detauls options
+        oData = $.extend({}, oDefaults, oData);
+        return this.each(function () {
+            if (oData.text) {
+                self.show($(this), oData);
+            }
+        });
+    };
+
+
+}(jQuery));
